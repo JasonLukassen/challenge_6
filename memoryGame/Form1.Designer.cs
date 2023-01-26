c9d6e4c7f1442e554fcdcb28edb7352514c072d6
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2023-01-26 15:06:38 +0100

    extra push

--- memoryGame/Form1.Designer.cs
+++ memoryGame/Form1.Designer.cs
@@ -459,7 +459,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1902, 1033);
-            this.ControlBox = false;
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
